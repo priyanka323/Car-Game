de868aedd3b6afcd35e81380ac3e4452539fb8ff
priyanka323 <priyarankawat88160@gmail.com>
2022-02-14 09:08:27 -0800

    Car-agme

--- script.js
+++ script.js
@@ -9,22 +9,85 @@ let keys = {
     ArrowRight: false
 }
 
+let player = {
+    speed: 5,
+
+}
+
+
 startScreen.addEventListener("click", startGame)  
 document.addEventListener("keydown", KeyPress)
 document.addEventListener("keyup", KeyReleased)
 
+function playGame(){
+    console.log("in the game");
+    let car = document.querySelector('.car');
+
+    let road = gameArea.getBoundingClientRect();
+
+    if(player.start == true){
+        if(keys.ArrowUp && player.y>road.top){
+            
+            player.y = player.y-player.speed;
+        }
+        if(keys.ArrowDown && player.y<road.bottom){
+            player.y = player.y+player.speed;
+        }
+        if(keys.ArrowLeft && player.x>road.left){
+            player.x = player.x-player.speed;
+        }
+        if(keys.ArrowRight && player.x<road.width-50){
+            player.x = player.x+player.speed;
+        }
+        car.style.left = player.x + 'px';
+        car.style.top = player.y + 'px';
+        window.requestAnimationFrame(playGame);
+    }
+}
+
 function KeyPress(e){
     //console.log(e);
+    if(e.key == "ArrowUp" || e.key == "ArrowDown" || e.key == "ArrowLeft" || e.key == "ArrowRight")
+        
     keys[e.key] = true;
-    console.log(keys);
+    //console.log(keys);
     //console.log("Key pressed", e.key);
 }
 function KeyReleased(e){
+    if(e.key == "ArrowUp" || e.key == "ArrowDown" || e.key == "ArrowLeft" || e.key == "ArrowRight")
+    
     keys[e.key] = false;
-    console.log(keys);
+    //console.log(keys);
     //console.log("Key Released",e.key);
 }
 
 function startGame(){
-    console.log("Hey Game Started");
+    //console.log("Hey Game Started");
+    //added and removed classes
+    startScreen.classList.add('hide');
+    gameArea.classList.remove('hide');
+
+    for(let x = 0; x < 10; x++){
+        let div = document.createElement('div');
+        div.classList.add('line');
+        div.y = x * 150;
+        div.style.top = (x*150)+"px";
+        gameArea.appendChild(div)
+    }
+
+
+    player.start = true
+    window.requestAnimationFrame(playGame);
+    //startScreen.classList.add('hide');
+    //https://developer.mozilla.org/en-US/docs/Web/API/Window/requestAnimationFrame
+
+    //create car element
+    let car = document.createElement('div');
+    car.innerText = "Car";
+    //setup class to car element
+    car.setAttribute("class", "car");
+    gameArea.appendChild(car);
+    player.x = car.offsetLeft;
+    player.y = car.offsetTop;
+    console.log(player);
 }
